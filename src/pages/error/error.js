46f5c70775da2135ba8f617735f7622d9994ff50
Daniil Mironenko <dan.mironenko@gmail.com>
2024-09-28 22:59:17 +0300

    add error page

---
+++ src/pages/error/error.js
@@ -0,0 +1,13 @@
+import { View } from '../../view.js';
+
+export class ErrorView extends View {
+    constructor(router) {
+        super(router);
+        this.root = document.querySelector('#root');
+    }
+
+    render(message = 'Что-то пошло не так') {
+        const template = Handlebars.templates['error.hbs'];
+        this.root.innerHTML = template({ message });
+    }
+}
