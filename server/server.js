904a8f305cb2bb4cf0f71fe4baf1f129d533f3ed
MatiXxD <ilya_dinesenko@mail.ru>
2024-09-29 23:43:44 +0300

    Done auth page

--- server/server.js
+++ server/server.js
@@ -1,26 +1,25 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const publicDirs = ["public", "src", "node_modules"];
+const publicDirs = ['public', 'src', 'node_modules'];
 publicDirs.forEach((dir) => {
-  app.use(express.static(path.join(__dirname, "..", dir)));
+	app.use(express.static(path.join(__dirname, '..', dir)));
 });
-app.use(express.static(path.resolve(__dirname, "images")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 app
-  .listen(PORT, () => {
-    console.log(`server is running and listening on port ${PORT}`);
-  })
-  .on("error", (err) => {
-    console.error(`failed to start server: ${err.message}`);
-  });
+	.listen(PORT, () => {
+		console.log(`server is running and listening on port ${PORT}`);
+	})
+	.on('error', (err) => {
+		console.error(`failed to start server: ${err.message}`);
+	});
