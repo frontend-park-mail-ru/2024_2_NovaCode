d8094cbefaaa6d051a83e235f1784ed0bb0ce7fe
Daniil Mironenko <dan.mironenko@gmail.com>
2024-09-29 11:24:53 +0300

    fix ci workflow

--- .github/linters/eslint.config.mjs
+++ .github/linters/eslint.config.mjs
@@ -3,6 +3,10 @@ import pluginJs from "@eslint/js";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
   pluginJs.configs.recommended,
 ];
