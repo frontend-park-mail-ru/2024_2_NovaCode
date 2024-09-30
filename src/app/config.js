e34c37b3cf3435c47241bfee9f3f324fadea9b1f
Daniil Mironenko <dan.mironenko@gmail.com>
2024-09-30 06:51:49 +0300

    switch to dev config

--- src/app/config.js
+++ src/app/config.js
@@ -3,8 +3,8 @@ import { LoginView } from "../pages/login/login.js";
 import { SignupView } from "../pages/signup/signup.js";
 import { FeedView } from "../pages/feed/feed.js";
 
-export const API_URL = "http://185.241.194.24:8080";
-// export const API_URL = "http://localhost:8080";
+// export const API_URL = "http://185.241.194.24:8080";
+export const API_URL = "http://localhost:8080";
 
 export const LAYOUT = [HeaderView];
 
