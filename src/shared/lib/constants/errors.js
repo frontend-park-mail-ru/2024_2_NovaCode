9fff0049b21d54e455e2cf2eaa6043d9b181656c
Daniil Mironenko <dan.mironenko@gmail.com>
2024-10-20 00:57:40 +0300

    add new constants

---
+++ src/shared/lib/constants/errors.js
@@ -0,0 +1,5 @@
+export const PUBLIC_ERRORS = {
+    UNKNOWN: 'Возникла неизвестная ошибка!',
+    INVALID_USERNAME_OR_PASSWORD: 'Неверное имя или пароль!',
+    USER_EXISTS: 'Пользователь с таким именем или почтой уже существует!',
+}
