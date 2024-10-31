6b7ea24f0415ee488647ae06d0f5e4f0422ef158
Daniil Mironenko <dan.mironenko@gmail.com>
2024-10-31 14:55:19 +0300

    add link handler for 'a' tag

---
+++ src/shared/lib/link.js
@@ -0,0 +1,8 @@
+import { eventBus } from "./eventbus.js";
+
+export const handleLink = (event) => {
+  event.preventDefault();
+  const href = event.target.getAttribute("href");
+  eventBus.emit("navigate", href);
+};
+
