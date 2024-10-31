deda20ed0c29d89101bb5c363e99d771e7426af3
Daniil Mironenko <dan.mironenko@gmail.com>
2024-10-31 15:04:29 +0300

    rename 'getStore' method to 'loadUser'

--- src/pages/artist/ui/Artist.js
+++ src/pages/artist/ui/Artist.js
@@ -1,39 +1,39 @@
-import { TrackListAPI } from '../../../widgets/trackList/index.js';
-import { ArtistCardView } from '../../../widgets/artistCard/index.js';
-import { TrackListView } from '../../../widgets/trackList/index.js';
-import { AlbumListView } from '../../../widgets/albumList/index.js';
-import { FooterPlayerView } from '../../../widgets/footerPlayer/index.js';
-import { userStore } from '../../../entities/user/model/store.js';
-import { player } from '../../../shared/player/model/store.js';
+import { TrackListAPI } from "../../../widgets/trackList/index.js";
+import { ArtistCardView } from "../../../widgets/artistCard/index.js";
+import { TrackListView } from "../../../widgets/trackList/index.js";
+import { AlbumListView } from "../../../widgets/albumList/index.js";
+import { FooterPlayerView } from "../../../widgets/footerPlayer/index.js";
+import { userStore } from "../../../entities/user/model/store.js";
+import { player } from "../../../shared/player/model/store.js";
 
 export class ArtistPage {
-	/**
-	 * Creates an instance of the View class.
-	 */
-	constructor(params) {
-		this.parent = document.querySelector('#root');
-		this.artistId = params['artistId'];
-	}
+  /**
+   * Creates an instance of the View class.
+   */
+  constructor(params) {
+    this.parent = document.querySelector("#root");
+    this.artistId = params["artistId"];
+  }
 
-	async render() {
-		this.parent.innerHTML = '';
+  async render() {
+    this.parent.innerHTML = "";
 
-		const trackListAPI = new TrackListAPI(this.artistId);
-		const artistCardView = new ArtistCardView(this.parent, this.artistId);
-		const trackListView = new TrackListView(this.parent, this.artistId);
-		const albumListView = new AlbumListView(this.parent, this.artistId);
-		const footPlayerView = new FooterPlayerView(this.parent);
+    const trackListAPI = new TrackListAPI(this.artistId);
+    const artistCardView = new ArtistCardView(this.parent, this.artistId);
+    const trackListView = new TrackListView(this.parent, this.artistId);
+    const albumListView = new AlbumListView(this.parent, this.artistId);
+    const footPlayerView = new FooterPlayerView(this.parent);
 
-		const tracks = await trackListAPI.get();
-		player.setTracks(tracks);
+    const tracks = await trackListAPI.get();
+    player.setTracks(tracks);
 
-		await artistCardView.render();
-		await trackListView.render(tracks);
-		await albumListView.render();
+    await artistCardView.render();
+    await trackListView.render(tracks);
+    await albumListView.render();
 
-		const user = userStore.getUser();
-		if (user) {
-			await footPlayerView.render();
-		}
-	}
+    const user = userStore.loadUser();
+    if (user) {
+      await footPlayerView.render();
+    }
+  }
 }
--- src/pages/feed/ui/Feed.js
+++ src/pages/feed/ui/Feed.js
@@ -1,38 +1,38 @@
-import { TrackListAPI } from '../../../widgets/trackList/index.js';
-import { ListenBlockView } from '../../../widgets/listenBlock/index.js';
-import { TrackListView } from '../../../widgets/trackList/index.js';
-import { ArtistListView } from '../../../widgets/artistList/index.js';
-import { FooterPlayerView } from '../../../widgets/footerPlayer/index.js';
-import { userStore } from '../../../entities/user/model/store.js';
-import { player } from '../../../shared/player/model/store.js';
+import { TrackListAPI } from "../../../widgets/trackList/index.js";
+import { ListenBlockView } from "../../../widgets/listenBlock/index.js";
+import { TrackListView } from "../../../widgets/trackList/index.js";
+import { ArtistListView } from "../../../widgets/artistList/index.js";
+import { FooterPlayerView } from "../../../widgets/footerPlayer/index.js";
+import { userStore } from "../../../entities/user/model/store.js";
+import { player } from "../../../shared/player/model/store.js";
 
 export class FeedPage {
-	/**
-	 * Creates an instance of the View class.
-	 */
-	constructor() {
-		this.parent = document.querySelector('#root');
-	}
+  /**
+   * Creates an instance of the View class.
+   */
+  constructor() {
+    this.parent = document.querySelector("#root");
+  }
 
-	async render() {
-		this.parent.innerHTML = '';
+  async render() {
+    this.parent.innerHTML = "";
 
-		const trackListAPI = new TrackListAPI();
-		const listenBlockView = new ListenBlockView(this.parent);
-		const trackListView = new TrackListView(this.parent);
-		const artistListView = new ArtistListView(this.parent);
-		const footPlayerView = new FooterPlayerView(this.parent);
+    const trackListAPI = new TrackListAPI();
+    const listenBlockView = new ListenBlockView(this.parent);
+    const trackListView = new TrackListView(this.parent);
+    const artistListView = new ArtistListView(this.parent);
+    const footPlayerView = new FooterPlayerView(this.parent);
 
-		const tracks = await trackListAPI.get();
-		player.setTracks(tracks);
+    const tracks = await trackListAPI.get();
+    player.setTracks(tracks);
 
-		await listenBlockView.render();
-		await trackListView.render(tracks);
-		await artistListView.render();
+    await listenBlockView.render();
+    await trackListView.render(tracks);
+    await artistListView.render();
 
-		const user = userStore.getUser();
-		if (user) {
-			await footPlayerView.render();
-		}
-	}
+    const user = userStore.loadUser();
+    if (user) {
+      await footPlayerView.render();
+    }
+  }
 }
