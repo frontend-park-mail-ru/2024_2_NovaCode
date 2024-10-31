9bfd24577858e97fd1c588fbd81ddf024f5c715e
Daniil Mironenko <dan.mironenko@gmail.com>
2024-10-31 14:59:43 +0300

    add public errors for user

--- src/shared/lib/constants/errors.js
+++ src/shared/lib/constants/errors.js
@@ -2,4 +2,7 @@ export const PUBLIC_ERRORS = {
     UNKNOWN: 'Возникла неизвестная ошибка!',
     INVALID_USERNAME_OR_PASSWORD: 'Неверное имя или пароль!',
     USER_EXISTS: 'Пользователь с таким именем или почтой уже существует!',
+    UNAUTHORIZED: 'Вы не авторизованы!',
+    FORBIDDEN: 'У вас нет доступа к этой странице/действию!',
+    BAD_REQUEST: 'Внутренняя ошибка!',
 }
