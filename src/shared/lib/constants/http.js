d4652f707d73ba3b4c8f1242daacda17a7b1494d
Daniil Mironenko <dan.mironenko@gmail.com>
2024-10-31 15:00:11 +0300

    http status constants

--- src/shared/lib/constants/http.js
+++ src/shared/lib/constants/http.js
@@ -13,4 +13,7 @@ export const API_ENDPOINTS = {
     SIGN_IN: `${API_URL}/api/v1/auth/login`,
     SIGN_UP: `${API_URL}/api/v1/auth/register`,
     SIGN_OUT: `${API_URL}/api/v1/auth/logout`,
+    GET_USER: (username) => `${API_URL}/api/v1/users/${username}`,
+    UPDATE_USER: (userID) => `${API_URL}/api/v1/users/${userID}`,
+    UPLOAD_AVATAR: (userID) => `${API_URL}/api/v1/users/${userID}/image`,
 };
