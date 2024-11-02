dec21cfffbe7ebd6acafe467c633295e02333c4b
MatiXxD <ilya_dinesenko@mail.ru>
2024-11-02 22:24:18 +0300

    Done profile page

--- src/widgets/userCard/ui/userCard.js
+++ src/widgets/userCard/ui/userCard.js
@@ -1,34 +1,35 @@
-import { userStore } from "../../../entities/user/index.js";
-import { handleLink } from "../../../shared/lib/index.js";
+import { userStore } from '../../../entities/user/index.js';
+import { handleLink } from '../../../shared/lib/index.js';
 
 export class UserCardView {
-  parent;
-  username;
+	parent;
+	username;
 
-  constructor(parent, username) {
-    this.parent = parent ? parent : document.querySelector("#root");
-    this.username = username;
-  }
+	constructor(parent, username) {
+		this.parent = parent ? parent : document.querySelector('#root');
+		this.username = username;
+	}
 
-  async render() {
-    let user = await userStore.getUser(this.username);
+	async render() {
+		let user = await userStore.getUser(this.username);
+		console.log(user);
 
-    const isCurrentUser = userStore.storage.user.username === this.username;
+		const isCurrentUser = userStore.storage.user.username === this.username;
 
-    const template = Handlebars.templates["userCard.hbs"];
+		const template = Handlebars.templates['userCard.hbs'];
 
-    const userCardElement = document.createElement("div");
-    userCardElement.classList.add("user_card");
-    userCardElement.innerHTML = template({ user, isCurrentUser });
-    this.parent.appendChild(userCardElement);
+		const userCardElement = document.createElement('div');
+		userCardElement.classList.add('user_card');
+		userCardElement.innerHTML = template({ user, isCurrentUser });
+		this.parent.appendChild(userCardElement);
 
-    this.bindEvents();
-  }
+		this.bindEvents();
+	}
 
-  bindEvents() {
+	bindEvents() {
 		const links = this.parent.querySelectorAll('.link');
-		links.forEach(link => {
-		  	link.addEventListener('click', handleLink);
+		links.forEach((link) => {
+			link.addEventListener('click', handleLink);
 		});
 	}
 }
