37d2b2821ca37c2600c23fcd57279b8d6bb49b01
Tatiana Bocharova <tanka.bocharova.23@gmail.com>
2024-11-19 11:10:38 +0300

    add album page

--- src/entities/album/ui/album.js
+++ src/entities/album/ui/album.js
@@ -1,3 +1,4 @@
+import { eventBus } from '../../../shared/lib/index.js';
 import { S3_BUCKETS } from "../../../shared/lib/index.js";
 import template from './album.hbs';
 import './album.scss';
@@ -15,19 +16,41 @@ export class AlbumView {
 	 */
 	constructor(parent) {
 		this.parent = parent ?? document.querySelector('#root');
+
+		this.handleAlbumClick = this.handleAlbumClick.bind(this);
 	}
 
 	/**
 	 * Renders the album view.
 	 */
 	render(album) {
+		this.albumId = album.id;
+
 		if (album.image) {
 			album.image = `${S3_BUCKETS.ALBUM_IMAGES}/${album.image}`;
 		}
 
 		const albumElement = document.createElement('div');
 		albumElement.classList.add('album');
+		albumElement.setAttribute('data-id', this.albumId);
 		albumElement.innerHTML = template(album);
 		this.parent.appendChild(albumElement);
+		this.bindEvents();
+	}
+
+	bindEvents() {
+		const album = this.parent.querySelector(`[data-id="${this.albumId}"]`);
+		album?.addEventListener('click', this.handleAlbumClick);
+	}
+
+	handleAlbumClick() {
+		eventBus.emit('navigate', `/album/${this.albumId}`);
+	}
+
+	destructor() {
+		const album = this.parent.querySelector(`[data-id="${this.albumId}"]`);
+		if (album) {
+			album.removeEventListener('click', this.handleAlbumClick);
+		}
 	}
 }
