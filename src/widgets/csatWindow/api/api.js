7f9b21d4a6c409c39e1fdfb1963a8aa01a0b2649
MatiXxD <ilya_dinesenko@mail.ru>
2024-11-23 16:09:44 +0300

    Add api for iframe

---
+++ src/widgets/csatWindow/api/api.js
@@ -0,0 +1,39 @@
+import { API_URL } from '../../../shared/config/index.js';
+import { GET, POST } from '../../../shared/api/index.js';
+
+export class TrackListAPI {
+	/**
+	 * Url path to backend api which returns tracks
+	 *
+	 */
+	url;
+
+	constructor() {
+		this.url = `${API_URL}/api/v1/csat/questions`;
+	}
+
+	async getQuestions() {
+		try {
+			const response = await GET(this.url);
+			if (!response.error) {
+				return response.data;
+			} else {
+				console.log('Error during questions');
+				return [];
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	async addScore(questionID) {
+		try {
+			const response = await POST(`${this.url}/${questionID}/submit`);
+			if (response.error) {
+				console.log('Error during add favorite track:');
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	}
+}
--- src/widgets/csatWindow/ui/csatWindow.js
+++ src/widgets/csatWindow/ui/csatWindow.js
@@ -3,33 +3,36 @@ import './csatWindow.scss';
 import styles from './csatWindow.scss?inline';
 
 export class CSATWindow {
-    constructor(parent) {
-        this.parent = parent ? parent : document.querySelector("#root");
-        this.text = 'text';
-    }
+	constructor(parent) {
+		this.parent = parent ? parent : document.querySelector('#root');
+		this.text = 'text';
+	}
 
-    render() {
-        this.csatWindowIframe = document.createElement("iframe");
-        this.csatWindowIframe.classList.add('csat-iframe');
-        this.parent.appendChild(this.csatWindowIframe);
+	render() {
+		this.csatWindowIframe = document.createElement('iframe');
+		this.csatWindowIframe.classList.add('csat-iframe');
+		this.parent.appendChild(this.csatWindowIframe);
 
-        this.iframeDoc = this.csatWindowIframe.contentWindow.document;
+		this.iframeDoc = this.csatWindowIframe.contentWindow.document;
 
-        console.log(styles);
-        const style = this.iframeDoc.createElement('style');
-        style.innerHTML = styles;
-        this.iframeDoc.head.appendChild(style);
+		const style = this.iframeDoc.createElement('style');
+		style.innerHTML = styles;
+		this.iframeDoc.head.appendChild(style);
 
-        const div = document.createElement('div');
-        div.classList.add("csat-window");
-        div.innerHTML = template({ text: this.text });
+		const div = document.createElement('div');
+		div.classList.add('csat-window');
+		div.innerHTML = template({ text: this.text });
 
-        this.iframeDoc.body.appendChild(div);
+		this.iframeDoc.body.appendChild(div);
 
-        const btnNo = this.iframeDoc.documentElement.querySelector('.csat-window__btn_no');
-        btnNo.addEventListener('click', () => alert("no works!"));
+		const btnNo = this.iframeDoc.documentElement.querySelector(
+			'.csat-window__btn_no',
+		);
+		btnNo.addEventListener('click', () => alert('no works!'));
 
-        const btnYes = this.iframeDoc.documentElement.querySelector('.csat-window__btn_yes');
-        btnYes.addEventListener('click', () => alert("yes works!"));
-    }
+		const btnYes = this.iframeDoc.documentElement.querySelector(
+			'.csat-window__btn_yes',
+		);
+		btnYes.addEventListener('click', () => alert('yes works!'));
+	}
 }
