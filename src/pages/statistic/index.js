6bafbd683df68c23b6679d1fb1ea21285b5620e8
Tatiana Bocharova <tanka.bocharova.23@gmail.com>
2024-11-23 14:43:06 +0300

    statistic page

---
+++ src/pages/statistic/index.js
@@ -0,0 +1,1 @@
+export { StatisticPage } from "./ui/Statistic.js";
---
+++ src/pages/statistic/ui/Statistic.js
@@ -0,0 +1,30 @@
+// import { FooterPlayerView } from '../../../widgets/footerPlayer/index.js';
+// import { userStore } from '../../../entities/user/model/store.js';
+// import { player } from '../../../shared/player/model/store.js';
+import { StatisticListView } from '../../../widgets/statisticList/index.js'
+import template from './Statistic.hbs';
+import './Statistic.scss';
+
+export class StatisticPage {
+	/**
+	 * Creates an instance of the View class.
+	 */
+	constructor() {
+		this.parent = document.querySelector('#root');
+	}
+
+	async render() {
+		this.parent.innerHTML = '';
+
+		this.parent.innerHTML = template();
+
+        const statisticListView = new StatisticListView();
+        await statisticListView.render();
+
+		// const footPlayerView = new FooterPlayerView(this.parent);
+		// const user = userStore.storage.user;
+		// if (user) {
+		// 	await footPlayerView.render();
+		// }
+	}
+}
