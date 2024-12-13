f57217af11d5f34f4fc249b94577a377522737f9
Daniil Mironenko <dan.mironenko@gmail.com>
2024-12-13 23:55:33 +0300

    update api config

--- src/shared/config/api.js
+++ src/shared/config/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://nova-music.ru";
+// export const BASE_URL = "http://localhost:8080";
 
 // export const API_USER_URL = "http://localhost:8080";
 // export const API_ALBUM_URL = "http://localhost:8080";
@@ -9,11 +9,13 @@ export const BASE_URL = "https://nova-music.ru";
 // export const API_CSAT_URL = "http://localhost:8080";
 // export const S3_URL = "http://localhost:8010";
 
-export const API_USER_URL = 'https://nova-music.ru:8080';
-export const API_ALBUM_URL = 'https://nova-music.ru:8080';
-export const API_ARTIST_URL = 'https://nova-music.ru:8080';
-export const API_PLAYLIST_URL = 'https://nova-music.ru:8080';
-export const API_TRACK_URL = 'https://nova-music.ru:8080';
-export const API_GENRE_URL = 'https://nova-music.ru:8080';
-export const API_CSAT_URL = 'https://nova-music.ru:8080';
-export const S3_URL = 'https://nova-music.ru:8010';
+export const BASE_URL = "https://nova-music.ru";
+
+export const API_USER_URL = "https://nova-music.ru";
+export const API_ALBUM_URL = "https://nova-music.ru";
+export const API_ARTIST_URL = "https://nova-music.ru:";
+export const API_PLAYLIST_URL = "https://nova-music.ru";
+export const API_TRACK_URL = "https://nova-music.ru";
+export const API_GENRE_URL = "https://nova-music.ru";
+export const API_CSAT_URL = "https://nova-music.ru";
+export const S3_URL = "https://nova-music.ru:8010";
