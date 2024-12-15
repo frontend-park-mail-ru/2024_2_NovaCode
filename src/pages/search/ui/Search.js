9b5585d80c7400f8f7326cf5aba0b86a795f9e59
MatiXxD <ilya_dinesenko@mail.ru>
2024-12-15 22:13:10 +0300

    Add player on search page

--- src/pages/search/ui/Search.js
+++ src/pages/search/ui/Search.js
@@ -1,57 +1,66 @@
-import { eventBus } from '../../../shared/lib/index.js';
-import { SearchLineView } from '../../../widgets/searchLine/index.js';
-import { ArtistListView } from '../../../widgets/artistList/index.js';
-import { TrackListView } from '../../../widgets/trackList/index.js';
-import { AlbumListView } from '../../../widgets/albumList/index.js';
-import { ErrorView } from '../../../widgets/error/index.js';
+import { SearchLineView } from "../../../widgets/searchLine/index.js";
+import { ArtistListView } from "../../../widgets/artistList/index.js";
+import { TrackListView } from "../../../widgets/trackList/index.js";
+import { AlbumListView } from "../../../widgets/albumList/index.js";
+import { ErrorView } from "../../../widgets/error/index.js";
+import { player } from "../../../shared/player/model/store.js";
+import { eventBus } from "../../../shared/lib/index.js";
+import { userStore } from "../../../entities/user/index.js";
 
 export class SearchPage {
   /**
    * Creates an instance of the View class.
    */
   constructor() {
-    this.parent = document.querySelector('#root');
+    this.parent = document.querySelector("#root");
     this.eventHandlers = {
       foundArtists: this.handleFoundArtists.bind(this),
       foundAlbums: this.handleFoundAlbums.bind(this),
       foundTracks: this.handleFoundTracks.bind(this),
-      emptySearchResult: this.handleNotFound.bind(this)
+      emptySearchResult: this.handleNotFound.bind(this),
     };
   }
 
   async render() {
-    this.parent.innerHTML = '';
+    this.parent.innerHTML = "";
 
-		this.pageContent = document.createElement('div');
-		this.pageContent.classList.add('page_content');
-		this.parent.appendChild(this.pageContent);
+    this.pageContent = document.createElement("div");
+    this.pageContent.classList.add("page_content");
+    this.parent.appendChild(this.pageContent);
 
-		const searchLine = new SearchLineView(this.pageContent);
+    const searchLine = new SearchLineView(this.pageContent);
     await searchLine.render();
 
     this.onEvents();
-    eventBus.emit('hidePlayer');
+    if (
+      userStore.storage.user.isAuthorized &&
+      (player.isLoaded || player.isPlaying)
+    ) {
+      eventBus.emit("showPlayer");
+    } else {
+      eventBus.emit("hidePlayer");
+    }
   }
-  
+
   onEvents() {
-    Object.keys(this.eventHandlers).forEach(event => {
+    Object.keys(this.eventHandlers).forEach((event) => {
       eventBus.on(event, this.eventHandlers[event]);
     });
   }
-  
+
   offEvents() {
-    Object.keys(this.eventHandlers).forEach(event => {
+    Object.keys(this.eventHandlers).forEach((event) => {
       eventBus.off(event, this.eventHandlers[event]);
     });
   }
 
   async handleFoundArtists(artists) {
-    const errorElement = document.querySelector('.error');
+    const errorElement = document.querySelector(".error");
     if (errorElement) {
       errorElement.remove();
     }
 
-    const artistsElement = document.querySelector('.artists');
+    const artistsElement = document.querySelector(".artists");
     if (artistsElement) {
       artistsElement.remove();
     }
@@ -65,12 +74,12 @@ export class SearchPage {
   }
 
   async handleFoundAlbums(albums) {
-    const errorElement = document.querySelector('.error');
+    const errorElement = document.querySelector(".error");
     if (errorElement) {
       errorElement.remove();
     }
-  
-    const albumsElement = document.querySelector('.albums');
+
+    const albumsElement = document.querySelector(".albums");
     if (albumsElement) {
       albumsElement.remove();
     }
@@ -84,12 +93,16 @@ export class SearchPage {
   }
 
   async handleFoundTracks(tracks) {
-    const errorElement = document.querySelector('.error');
+    if (tracks.length > 0) {
+      player.addTracks(tracks);
+    }
+
+    const errorElement = document.querySelector(".error");
     if (errorElement) {
       errorElement.remove();
     }
 
-    const tracksElement = document.querySelector('.tracks');
+    const tracksElement = document.querySelector(".tracks");
     if (tracksElement) {
       tracksElement.remove();
     }
@@ -103,12 +116,16 @@ export class SearchPage {
   }
 
   async handleNotFound() {
-    const errorElement = document.querySelector('.error');
+    const errorElement = document.querySelector(".error");
     if (errorElement) {
       errorElement.remove();
     }
 
-    const errorView = new ErrorView(null, 'Ничего не найдено', 'Попробуйте поискать что-то другое.');
+    const errorView = new ErrorView(
+      null,
+      "Ничего не найдено",
+      "Попробуйте поискать что-то другое.",
+    );
     await errorView.render();
   }
 
